d61723f3c8d81cbc6b6b779b4d9feaddd96507f1
SabanaSultana <sabanasultana132@gamil.com>
2026-01-16 18:35:56 +0530

    update: main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 import json
+from fastapi import HTTPException as http 
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Hello, World!", "number":910}
+    return {"message": "Hello, World!", "number":910000}
 
 def additional_function():
     return "This is an additional function." # This function does not include a route decorator and will not be exposed as an endpoint.
@@ -22,7 +23,7 @@ def about():
 def load_patientData():
     with open('patient_info.json', 'r') as file:
         data=json.load(file)
-        # return data
+        return data
     print("Patient data loaded.")
     print("This is out data",data)
 
@@ -30,4 +31,36 @@ def load_patientData():
 def get_patient():
     return load_patientData()
 
-print(load_patientData())
+# print(load_patientData())
+
+
+## Parameterized Endpoint to get patient by ID
+
+@app.get("/patient/{patient_id}")
+def get_patient_by_id(patient_id:int =Path(..., description="The ID of the patient to retrieve", example=1)):
+    data=load_patientData()
+    print("Data in get_patient_by_id:",data)
+    patients=data['patients']
+    print("Patient data:",patients)
+    for patient in patients:
+        if patient['id']==patient_id:
+            print("Patient found:",patient)
+            return patient
+        
+    # return {"message": "Patient not found"}
+    raise http(status_code=404, detail="Patient not found")
+
+
+## Query paramater to sort by age descending
+
+@app.get("/patients_sorted")
+def get_patients_sorted_by_age(sort : str=None):
+    data=load_patientData()
+    patient=data['patients']
+    if sort =="desc":
+        sorted_patients=sorted(patient, key= lambda x:x['age'], reverse=True)
+        return {"patients": sorted_patients}
+    else:
+        return {"patients": patient}
+    
+# Example usage: /patients_sorted?sort=desc to get patients sorted by age in descending order
